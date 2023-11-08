63298cfe737228b678fb1ea6270cf7259e7381ec
dhruveel10 <66111178+dhruveel10@users.noreply.github.com>
2023-11-08 16:36:37 -0500

    Update MainActivity.kt

--- app/src/main/java/edu/vt/cs5254/dreamcatcher/MainActivity.kt
+++ app/src/main/java/edu/vt/cs5254/dreamcatcher/MainActivity.kt
@@ -7,9 +7,6 @@ import edu.vt.cs5254.dreamcatcher.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    //Name: Dhruveel Chouhan
-    //pid: dhruveel10
-
     private  lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
